d681f7301c879424d77558bc877442fcb1fef10b
Vinni Hoke <vinni.hoke@gmail.com>
2019-12-16 20:53:40 -0800

    First Push

---
+++ api/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const schema = require("./schema.js");
+
+const server = express()
+
+
+server.use(helmet());
+server.use(morgan('dev'));
+server.use(express).json();
+
+server.use('/graphql', graphqlHTTP({
+	schema,
+	graphiql: true
+}));
+
